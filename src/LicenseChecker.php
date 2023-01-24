99db18fe4617ddfed0a45ae880a40492c411e33e
Ben Challis <ben-challis@users.noreply.github.com>
2023-01-24 17:27:39 +0000

    Fix --allow-files definition to require a value (#15)

--- src/LicenseChecker.php
+++ src/LicenseChecker.php
@@ -18,7 +18,7 @@ final class LicenseChecker extends SingleCommandApplication
         $this
             ->setName('Composer License Checker')
             ->setVersion('0.0.1')
-            ->addOption('allow-file', 'a', InputOption::VALUE_OPTIONAL, 'Path to the allowed licenses configuration file', '.allowed-licenses.php');
+            ->addOption('allow-file', 'a', InputOption::VALUE_REQUIRED, 'Path to the allowed licenses configuration file', '.allowed-licenses.php');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
