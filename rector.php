7cb25d4d167658b9abf6205fc5f2bf8a00dc0a42
Ben Challis <ben@lendable.co.uk>
2023-02-21 08:36:37 +0000

    Remove unused import in rector.php

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
