a064b18f3b3ff498ac49042480246f881ac75ad3
Ben Challis <ben-challis@users.noreply.github.com>
2023-02-21 09:20:54 +0000

    Remove unused import in rector.php, run static analysis against rector.php (#63)

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
@@ -12,7 +11,7 @@ use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rector): void {
     $rector->parallel();
-    $rector->paths([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/bin']);
+    $rector->paths([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/bin', __DIR__.'/rector.php']);
     $rector->phpVersion(PhpVersion::PHP_81);
     $rector->phpstanConfig(__DIR__.'/phpstan-rector.neon');
     $rector->cacheDirectory(__DIR__.'/tmp/rector');
