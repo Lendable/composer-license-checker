2145d153787baf22c38c019eda110e269c7f93f0
Ben Challis <ben@lendable.co.uk>
2023-03-04 21:17:02 +0000

    Make return/throws easier from stubs

--- tests/support/StubComposerRunner.php
+++ tests/support/StubComposerRunner.php
@@ -5,34 +5,18 @@ declare(strict_types=1);
 namespace Tests\Support\Lendable\ComposerLicenseChecker;
 
 use Lendable\ComposerLicenseChecker\ComposerRunner;
-use Lendable\ComposerLicenseChecker\Exception\FailedRunningComposer;
 
 final class StubComposerRunner implements ComposerRunner
 {
-    private bool $willThrow = false;
-
-    public function __construct(
-        private string $output = '',
-    ) {
+    /**
+     * @param Result<string> $result
+     */
+    public function __construct(private readonly Result $result)
+    {
     }
 
     public function licenses(string $projectPath, bool $ignoreDev): string
     {
-        if ($this->willThrow) {
-            throw FailedRunningComposer::withCommand($this->output === '' ? 'unknown' : $this->output);
-        }
-
-        return $this->output;
-    }
-
-    public function setOutput(string $output): void
-    {
-        $this->output = $output;
-    }
-
-    public function willThrow(string $message = 'problem, officer?'): void
-    {
-        $this->willThrow = true;
-        $this->output = $message;
+        return $this->result->provide();
     }
 }
