ee3aefc58d53cab99f7aa54248203e0c134a1e4a
Marcin Michalski <evulmastah@gmail.com>
2023-06-20 18:21:49 +0100

    deprecated rule

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,7 +10,6 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
-        '@PHP80Migration' => true,
         '@PHP81Migration' => true,
         '@DoctrineAnnotation' => true,
         'align_multiline_comment' => true,
@@ -18,6 +17,7 @@ return (new PhpCsFixer\Config())
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => true,
         'blank_line_after_opening_tag' => true,
+        'blank_lines_before_namespace' => true,
         'blank_line_before_statement' => true,
         'cast_spaces' => ['space' => 'single'],
         'class_attributes_separation' => true,
@@ -96,7 +96,6 @@ return (new PhpCsFixer\Config())
         'return_type_declaration' => true,
         'semicolon_after_instruction' => true,
         'short_scalar_cast' => true,
-        'single_blank_line_before_namespace' => true,
         'single_line_comment_style' => false,
         'single_quote' => true,
         'space_after_semicolon' => true,
