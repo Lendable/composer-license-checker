f008906ef6864758a23924d23ad6f270a34f1ffc
Ben Challis <ben@lendable.co.uk>
2024-01-23 22:21:15 +0000

    Fix Rector deprecation

--- rector.php
+++ rector.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rector): void {
@@ -19,8 +19,8 @@ return static function (RectorConfig $rector): void {
     $rector->sets([
         SetList::CODE_QUALITY,
         LevelSetList::UP_TO_PHP_81,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-        SymfonyLevelSetList::UP_TO_SYMFONY_54,
+        PHPUnitSetList::PHPUNIT_100,
+        SymfonySetList::SYMFONY_54,
     ]);
     $rector->skip([
         FlipTypeControlToUseExclusiveTypeRector::class,
