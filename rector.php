bb64c27e590d3aef06ea5fd301473accea534503
Ben Challis <ben@lendable.co.uk>
2024-03-01 10:25:04 +0000

    Use v1 style of Rector configuration

--- rector.php
+++ rector.php
@@ -2,27 +2,26 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rector): void {
-    $rector->parallel();
-    $rector->paths([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/bin', __DIR__.'/rector.php']);
-    $rector->phpVersion(PhpVersion::PHP_81);
-    $rector->phpstanConfig(__DIR__.'/phpstan-rector.neon');
-    $rector->cacheDirectory(__DIR__.'/tmp/rector');
-    $rector->sets([
-        SetList::CODE_QUALITY,
-        LevelSetList::UP_TO_PHP_81,
-        PHPUnitSetList::PHPUNIT_100,
-        SymfonySetList::SYMFONY_54,
-    ]);
-    $rector->skip([
-        FlipTypeControlToUseExclusiveTypeRector::class,
-    ]);
-};
+return RectorConfig::configure()
+    ->withPaths([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/bin', __DIR__.'/rector.php'])
+    ->withPHPStanConfigs([__DIR__.'/phpstan-rector.neon'])
+    ->withCache(__DIR__.'/tmp/rector', FileCacheStorage::class)
+    ->withPreparedSets(codeQuality: true)
+    ->withPhpSets(php81: true)
+    ->withSets(
+        [
+            PHPUnitSetList::PHPUNIT_100,
+            SymfonySetList::SYMFONY_54,
+        ],
+    )
+    ->withSkip(
+        [
+            FlipTypeControlToUseExclusiveTypeRector::class,
+        ],
+    );
